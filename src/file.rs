dc00e6faacbe30c1c3dd6f61ace3a0800ea4081b
Clath <83691655+imupdating@users.noreply.github.com>
2022-12-23 19:11:54 +0800

    Update file.rs

--- src/file.rs
+++ src/file.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::Read};
 
-pub fn read_all(path: String) -> String {
-    let mut f = File::open(&path).expect("文件打开失败");
+pub fn read_all(path: &String) -> String {
+    let mut f = File::open(path).expect("文件打开失败");
 
     let mut result = String::new();
     f.read_to_string(&mut result).expect("文件读取失败");
