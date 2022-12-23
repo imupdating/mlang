746090d861ddafa4ed8a9abd0397ede6273294bf
Clath <83691655+imupdating@users.noreply.github.com>
2022-12-23 19:02:15 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 use mlang::file;
 use mlang::lexer;
-use mlang::states;
 use mlang::runner;
 use mlang::parser;
 use std::collections::HashMap;
 
 fn main() {
-//    let source = file::read_all("test.ms".to_string());
-//    let lexer_ = lexer::Lexer::new(source);
-//    let tokens = lexer_.fetch_all();
-//    println!("{:#?}", tokens);
-
-    // let states = vec![states::States::NewFunction("test".to_string(), vec!["a".to_string()], vec![]),
-    //      states::States::CallFunction("test".to_string())];
-    
+    // 从文件读入源代码
     let source = file::read_all("test.ms".to_string());
+
+    // 词法分析
     let lexer_ = lexer::Lexer::new(source);
     let tokens = lexer_.fetch_all();
     println!("{:#?}", tokens);
+
+    // 语法分析
     let parser_ = parser::Parser::new(tokens.unwrap());
-    let mut states = parser_.fetch_all().unwrap();
-    states.push(states::States::CallFunction("add".to_string()));
+    let states = parser_.fetch_all().unwrap();
     println!("{:#?}", states);
+
+    // 运行代码
     let mut runner = runner::Runner::new(states, HashMap::new());
     runner.run();
     println!("{:#?}", runner.stack);
